f1c6be0f10d969ac5bbee0173f611492d09da65e
gearmamn06 <gearmamn06@gmail.com>
2019-02-18 01:02:23 +0900

    edit demo app (add clear animation)

--- AnimationSeries-Demo/AnimationSeries-Demo/ViewController.swift
+++ AnimationSeries-Demo/AnimationSeries-Demo/ViewController.swift
@@ -9,26 +9,50 @@
 import UIKit
 import AnimationSeries
 
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var animView: UIView!
     @IBOutlet weak var tableView: UITableView!
 
     private let types = AnimationExampleType.allCases.filter{ $0 != .none }
+    private var currentAnimations = [Recursable]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        initialAniamtion()
+        
+        startInitialAnim()
         setUpTableView()
     }
 
+}
+
+
+/// animation setting
+extension ViewController {
     
-    private func initialAniamtion() {
-        let anim = animView.sizing(scale: (40, 40), duration: 0) + animView.sizing(scale: (0.6, 0.6), duration: 1.6) + animView.sizing(scale: (1.0, 1.0), duration: 0.3)
+    private func clearCurrentAnimation() {
+        self.currentAnimations.forEach{ $0.clear() }
+        self.initializeView()
+    }
+    
+    private func initializeView() {
+        animView.transform = CGAffineTransform(rotationAngle: 0)
+        animView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        animView.transform = CGAffineTransform(translationX: 0, y: 0)
+        animView.alpha = 1.0
+        animView.backgroundColor = .red
+    }
+    
+    private func startInitialAnim() {
+        let anim = animView.sizing(scale: (40, 40), duration: 0) + animView.sizing(scale: (0.6, 0.6), duration: 1.6, { _ in print("shrink end..") }) + animView.sizing(scale: (1.0, 1.0), duration: 0.3)
+        anim.onNext = {
+            print("intial animation end...")
+        }
         anim.start()
     }
-
 }
 
 
@@ -54,8 +78,10 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let cell = tableView.cellForRow(at: indexPath) as? AnimListItemCell else { return }
-        let anims = cell.type.animate(self.animView)
-        anims.forEach { $0.start() }
+        
+        clearCurrentAnimation()
+        currentAnimations = cell.type.animate(self.animView)
+        currentAnimations.forEach { $0.start() }
     }
 }
 
