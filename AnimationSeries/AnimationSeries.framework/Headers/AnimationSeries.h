88b7bd17fd2f95cfc3ff60804639708a320be3f2
gearmamn06 <gearmamn06@gmail.com>
2019-02-20 01:46:37 +0900

    Universal target added + framework

---
+++ AnimationSeries/AnimationSeries.framework/Headers/AnimationSeries.h
@@ -0,0 +1,19 @@
+//
+//  AnimationSeries.h
+//  AnimationSeries
+//
+//  Created by ParkHyunsoo on 17/02/2019.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimationSeries.
+FOUNDATION_EXPORT double AnimationSeriesVersionNumber;
+
+//! Project version string for AnimationSeries.
+FOUNDATION_EXPORT const unsigned char AnimationSeriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimationSeries/PublicHeader.h>
+
+
